bae8af244b9f4091381f03e58726c5e2cdd62031
niezheng1 <niezheng1@jd.com>
2019-07-22 17:29:49 +0800

    项目调整

---
+++ dubbo-spi/src/main/java/com/zheng/nie/spi/java/Bumblebee.java
@@ -0,0 +1,14 @@
+package com.zheng.nie.spi.java;
+
+/**
+ * @author: niezheng1
+ * @Date: 2019/7/22 17:27
+ */
+public class Bumblebee implements Robot {
+
+    @Override
+    public void sayHello() {
+        System.out.println("Hello, I am Bumblebee.");
+    }
+
+}
---
+++ dubbo-spi/src/main/java/com/zheng/nie/spi/java/Robot.java
@@ -0,0 +1,9 @@
+package com.zheng.nie.spi.java;
+
+/**
+ * @author: niezheng1
+ * @Date: 2019/7/22 17:26
+ */
+public interface Robot {
+    void sayHello();
+}
