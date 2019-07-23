acb2125dbaf3e65e700166da8f102feaf5820cfe
niezheng1 <niezheng1@jd.com>
2019-07-23 09:14:48 +0800

    spi

---
+++ dubbo-spi/src/main/java/com/zheng/nie/spi/dubbo/DubboSPITest.java
@@ -0,0 +1,23 @@
+package com.zheng.nie.spi.dubbo;
+
+import com.zheng.nie.spi.java.Robot;
+import org.junit.Test;
+
+/**
+ * @author: niezheng1
+ * @Date: 2019/7/22 17:42
+ */
+public class DubboSPITest {
+
+
+//    @Test
+//    public void sayHello() throws Exception {
+//        ExtensionLoader<Robot> extensionLoader =
+//                ExtensionLoader.getExtensionLoader(Robot.class);
+//        Robot optimusPrime = extensionLoader.getExtension("optimusPrime");
+//        optimusPrime.sayHello();
+//        Robot bumblebee = extensionLoader.getExtension("bumblebee");
+//        bumblebee.sayHello();
+//    }
+
+}
---
+++ dubbo-spi/src/main/java/com/zheng/nie/spi/dubbo/WheelMaker.java
@@ -0,0 +1,8 @@
+package com.zheng.nie.spi.dubbo;
+
+/**
+ * @author: niezheng1
+ * @Date: 2019/7/22 17:50
+ */
+public interface WheelMaker {
+}
